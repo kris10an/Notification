10799ebd31f88e98ef7869fd7c75ba8b35c769ce
Kristian <kristian.hjelmeland@gmail.com>
2016-03-08 22:29:57 +0100

    plugin.py
    - Fortsatt med sendNotification
    - Lagt til config for variable folder i plugin config
    - forsøkt med sjekk av dette under startup, fører til at plugin ikke
    vil stoppe men må force quittes
    - oppsatert personsupdatePresence
    - Ny plugin id, opprinnelig var reservert av indigo / perceptive
    automation

--- Notification.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Notification.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -15,16 +15,37 @@ import strvartime
 # Note the "indigo" module is automatically imported and made available inside
 # our global name space by the host process.
 
+############################
+# Globals
+############################
+
+pluginId = u'com.kris10an.indigoplugin.notification'
+
+intervalToSeconds = {
+	u'1 hour'	:	60*60,
+	u'12 hours'	:	60*60*12,
+	u'1 day'		:	60*60*24,
+	u'1 week'	:	60*60*24*7,
+	u'1 month'	:	60*60*24*30 }
+
+
 ################################################################################
 class Plugin(indigo.PluginBase):
 	########################################
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+		
+		# Plugin prefs
 		self.debug = pluginPrefs.get(u'debugLog', False)
 		self.extDebug = pluginPrefs.get(u'extensiveDebug', False)
 		self.pluginLog = pluginPrefs.get(u'pluginLog', True)
+		self.varFolderName = pluginPrefs.get(u'varFolderName','Notification plugin log')
+		
+		# Error states
+		self.pluginConfigErrorState = False # Whether or not plugin is in error state, due to faulty plugin config
+		
+		# Presets, not configurable
 		self.notifyVarPrefix = u'notification'
-		self.notifyVarFolder = u'Notification log'
 		self.logDir = indigo.server.getInstallFolderPath() + u'/Logs/Notifications/'
 		self.logFileSuffix = u' Notifications.txt'
 		self.logFileDateFormat = u'%Y-%m-%d'
@@ -36,7 +57,23 @@ class Plugin(indigo.PluginBase):
 	########################################
 	def startup(self):
 		self.debugLog(u'Startup called')
-		self.debugLog(u'Log folder: %s' % self.logDir)
+		self.debugLog(u'Log folder: %s' % (self.logDir))
+		self.debugLog(u'Variable folder name: %s' % (self.varFolderName))
+		
+		# Check if our variable folder exists
+		if len(self.varFolderName) == 0:
+			# No folder name specified, quit plugin
+			self.pluginConfigErrorState = True
+			try:
+				while self.pluginConfigErrorState:
+					self.errorLog(u'No variable folder name specified in plugin config, please fix')
+					self.sleep(5)
+			except self.stopThread:
+				pass
+		
+# 		if not self.varFolderName in indigo.variables.folders:
+# 			self.debugLog(u'Variable folder "%s" not found, creating folder' % (self.varFolderName))
+# 			newFolder = indigo.variables.folder.create(self.varFolderName)
 
 	########################################
 	def shutdown(self):
@@ -44,9 +81,15 @@ class Plugin(indigo.PluginBase):
 		pass
 		
 	def closedPrefsConfigUi(self, valuesDict, userCancelled):
+		# Plugin prefs
 		self.debug = self.pluginPrefs.get("debugLog", False)
 		self.extDebug = self.pluginPrefs.get(u'extensiveDebug', False)
 		self.pluginLog = self.pluginPrefs.get("pluginLog", True)
+		self.varFolderName = self.pluginPrefs.get(u'varFolderName','Notification plugin log')
+		
+		# DO VALIDATION
+		self.pluginConfigErrorState = False
+		
 
 	########################################
 	# If runConcurrentThread() is defined, then a new thread is automatically created
@@ -88,14 +131,15 @@ class Plugin(indigo.PluginBase):
 		try:
 			categoryDev = indigo.devices[action.deviceId]
 		except:
-			self.errorLog(u"Invalid notification category device specified in send notification action")
+			self.errorLog(u'Invalid or unspecified notification category device specified in send notification action, dropping notification.\nNotification text: %s' % action.text)
 			return
 		
 		# Check if category device is enabled
 		if not categoryDev.enabled:
-			indigo.server.log(u'Notification category device "%s" has been disabled, skipping notification' % (categoryDev.name))
+			indigo.server.log(u'Notification category device "%s" has been disabled, skipping notification.\nNotification text: %s' % (categoryDev.name, action.text))
 			return
 			
+		actionProps = action.props
 		catProps = categoryDev.pluginProps
 		catStates = categoryDev.states
 		if self.extDebug: self.debugLog(u'Notification category "%s" plugin props: %s' % (categoryDev.name, str(catProps)))
@@ -104,40 +148,39 @@ class Plugin(indigo.PluginBase):
 		# Check if every notification is to be delivered, or by interval
 		send = False
 		timeNow = datetime.now()
+		
 		if(len(catStates[u'lastNotificationTime']) > 0):
-			try:
-				lastSent = strvartime.strToTime(catStates[u'lastNotificationTime'])
-				self.debugLog(u'Notification category last sent: %s' % (catStates[u'lastNotificationTime']))
-			except:
-				lastSent = strvartime.strToTime(u'2000-01-01 00:00:00')	
-				self.debugLog(u'Last notification time of category could not be obtatined, set to year 2000')		
-					
 			if catProps[u'sendEvery'] == u'always':
+				# Set to always send
 				send = True
 				self.debugLog(u'Notification category set to always send, will send now')
 			else:
-				#Determine if enough time has passed to send it again
-			
-				if catProps[u'sendEvery'] == u'1hour':
-					if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= 60*60: send = True
-				elif catProps[u'sendEvery'] == u'12hours':
-					if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= 60*60*12: send = True
-				elif catProps[u'sendEvery'] == u'1day':
-					if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= 60*60*24: send = True
-				elif catProps[u'sendEvery'] == u'1week':
-					if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= 60*60*24*7: send = True
-				elif catProps[u'sendEvery'] == u'1month':
-					if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= 60*60*24*30: send = True
+				# Notification category set to only send at given interval
+				# Check when notification category was last sent/used
+				try:
+					lastSent = strvartime.strToTime(catStates[u'lastNotificationTime'])
+					self.debugLog(u'Notification category last sent: %s' % (catStates[u'lastNotificationTime']))
+				except:
+					lastSent = strvartime.strToTime(u'2000-01-01 00:00:00')	
+				self.debugLog(u'Last notification time of category could not be obtatined, set to year 2000')
 				
-				self.debugLog(u'Notification category set to send every %s, Last notification sent %s. Send now: %s' % (catProps[u'sendEvery'], strvartime.prettyDate(lastSent), str(send)))
-	
+				#Determine if enough time has passed to send it again
+				if not catProps[u'sendEvery'] in intervalToSeconds:
+					self.errorLog(u'Invalid setting for delivery interval for category "%s". Check settings. Notification skipped.\nNotification text: %s' % (categoryDev.name, actionProps[u'text']))
+					return
+					
+				if strvartime.timeDiff(lastSent, timeNow, 'seconds') >= intervalToSeconds[catProps[u'sendEvery']]:
+					send = True
+					self.debugLog(u'Notification category set to send every %s, Last notification sent %s. Will send notification now' % (catProps[u'sendEvery'], strvartime.prettyDate(lastSent)))
+				else:
+					self.debugLog(u'Notification category set to send every %s, Last notification sent %s. Will _not_ send notification now' % (catProps[u'sendEvery'], strvartime.prettyDate(lastSent)))
 		else:
 			self.debugLog(u'Notification category has never been sent before, will send now')
 			send = True
 			
-		
 		#Make sure presence for persons is up to date
-		self.personsUpdatePresence()
+		if catProps[u'notifyPresent']:
+			numPresent = self.personsUpdatePresence(True)
 		
 		
 		
@@ -164,7 +207,7 @@ class Plugin(indigo.PluginBase):
 			indigo.server.log(u'Last notification time for device "%s" set to %s' % (dev.name, valuesDict[u'newValue']))
 			return True
 		except:
-			self.ErrorLog(u'Could not update last notification time manually')
+			self.errorLog(u'Could not update last notification time manually')
 			return
 	
 		
@@ -172,21 +215,27 @@ class Plugin(indigo.PluginBase):
 	# Update presence state of persons
 	########################################	
 	
-	def personsUpdatePresence( self ):
+	# errorIfNotSuccessful :	Sometimes may be useful not to throw errors if presence variable is not set/in use
+	
+	def personsUpdatePresence( self, errorIfNotSuccessful = True ):
 	
 		self.debugLog(u'Update presence for "person" devices')
+		i = 0 # Number of persons present
 		# iterate through "person" devices"
-		for dev in indigo.devices.iter('com.perceptiveautomation.indigoplugin.notification.notificationPerson'):
+		for dev in indigo.devices.iter(pluginId + u'.notificationPerson'):
 			devProps = dev.pluginProps
 			if self.extDebug: self.debugLog(u'"%s" plugin properties: %s' % (dev.name, str(devProps)))
 			try:
 				# Set state of presence based on variable given in device config UI
 				presenceVar = indigo.variables[int(devProps['presenceVariable'])]
-				dev.updateStateOnServer('present',str(presenceVar.getValue(bool, default=True)))
+				dev.updateStateOnServer('present',presenceVar.getValue(bool, default=True))
+				if presenceVar.getValue(bool, default=True): i = i + 1 # person is present
 				self.debugLog(u'"%s" person device presence set to: %s' % (dev.name, str(presenceVar.getValue(bool, default=True))))
 			except:
-				# Most likely no variable for presence is given, set to being present
-				dev.updateStateOnServer('present',str(presenceVar.getValue(bool, default=True)))
+				if errorIfNotSuccessful: self.errorLog(u'Could not get presence of person "%s", please check settings. Presence set to true as default' % dev.name)
+				# Most likely no variable for presence is given, set to being present as it's "safer" to receive notification than not
+				dev.updateStateOnServer('present',True)
+				i = i + 1
 				self.debugLog(u'"%s" person device presence set to true as no real presence could be obtained from variable' % (dev.name))
 				
 	########################################
